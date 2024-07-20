8e834ca3a2668b722b70e5eb047c6efa6c47a51b
seththomas <seth.thomas@emceestudios.com>
2024-07-19 21:25:30 -0400

    init

---
+++ src/main/java/com/seth/draft_assistant/DraftAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.seth.draft_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DraftAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DraftAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seth/draft_assistant/DraftAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seth.draft_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DraftAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
